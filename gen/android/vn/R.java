045407585548c4f38153ea7d6f0f92affd6516a9
dienkun <dangdinhdien.pc.no1@gmail.com>
2014-01-07 15:36:34 +0700

    delete error file
    dienkun

--- gen/android/vn/R.java
+++ gen/android/vn/R.java
@@ -46,6 +46,7 @@ public final class R {
         public static final int activity_main=0x7f030001;
         public static final int activity_marked=0x7f030002;
         public static final int activity_profile=0x7f030003;
+        public static final int main=0x7f030004;
     }
     public static final class menu {
         public static final int activity2=0x7f070000;
